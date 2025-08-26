caf35ddee9b640165d760655704c28225ea395a3
Gregory-Hu <greg502260171@gmail.com>
2025-08-26 18:33:37 +0800

    initial_repo

---
+++ logical/npu_mem/npu_ai_engine_lsu.sv
@@ -0,0 +1,12 @@
+
+
+
+
+
+
+
+// post process / activation 
+
+// simple post process
+
+// integer 
---
+++ logical/npu_top.sv
@@ -0,0 +1,51 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ----------------------------------------------------------
+// SOC Interface 
+//     - APB Command interface 
+//     - AXI (dedicated memory bandwidth)
+//     - local mmu
+//     - CHI Stash
+//     - interrupt
+
+
+// ----------------------------------------------------------
+// Scratch SRAM
+
+
+// ----------------------------------------------------------
+// NPU AI Engine (Matrix computation)
+//     - MAC Array
+//     - activation unit 
+//     - Load Store Unit
+//     - Transpose Unit (Pre Post)
+
+
+// ----------------------------------------------------------
+// USC_RVV Cores
+//     - Post Process Vector Computation
+//     - branching flow control
+//     - streaming vector processor 
+
+
+// ----------------------------------------------------------
+// Shared Cache
+//     - time locality data
+
+
+// ----------------------------------------------------------
+// do we need hts to dynamicly schedule work load 
+
